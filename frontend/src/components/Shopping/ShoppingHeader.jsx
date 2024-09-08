dd9454c4fd05e6892ecd462ddefa810cabc150f1
Himanshu Verma <kumarsurajverma6001@gmail.com>
2024-09-08 11:41:39 +0530

    Shopping Header Done

--- frontend/src/components/Shopping/ShoppingHeader.jsx
+++ frontend/src/components/Shopping/ShoppingHeader.jsx
@@ -1,11 +1,129 @@
-import React from 'react'
+import { shoppingViewHeaderMenuItems } from '@/config/Index';
+import { logoutUser } from '@/store/Slices/authSlice';
+import { DropdownMenuLabel } from '@radix-ui/react-dropdown-menu';
+import { House, LogOutIcon, Menu, ShoppingCart } from 'lucide-react';
+import { FaRegUserCircle } from 'react-icons/fa';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { Avatar, AvatarFallback } from '../ui/avatar';
+import { Button } from '../ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu';
+import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet';
+import { useToast } from '../ui/use-toast';
+
+const MenuItems = () => {
+  return (
+    <nav className=" poppins-medium flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
+      {shoppingViewHeaderMenuItems.map((item, index) => (
+        <Link
+          key={item.id}
+          to={item.path}
+          className=" text-sm font-medium hover:text-blue-700"
+        >
+          {' '}
+          {item.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+const HeaderRightContent = () => {
+  const { user } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { toast } = useToast();
+
+  const logoutHandler = async () => {
+    dispatch(logoutUser()).then((data) => {
+      if (data?.payload?.success) {
+        toast({
+          title: data?.payload?.message,
+        });
+      }
+    });
+  };
+
+  return (
+    <div className="flex lg:items-center lg:flex-row flex-col gap-4 poppins-medium">
+      <Button variant="outline" size="icon">
+        <ShoppingCart className=" w-6 h-6" />
+        <span className=" sr-only">User Cart</span>
+      </Button>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Avatar className=" bg-black">
+            <AvatarFallback className="bg-black text-white font-bold cursor-pointer">
+              {user?.username[0].toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent side="right" className="w-56">
+          <DropdownMenuLabel>
+            Logged in as
+            <span className=" text-lg font-medium"> {user?.username}</span>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem onClick={() => navigate('/shop/account')}>
+            <FaRegUserCircle className="w-4 h-4 mr-2" />
+            Account
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+
+          <DropdownMenuItem onClick={logoutHandler}>
+            <LogOutIcon className="w-4 h-4 mr-2" />
+            Logout
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
 
 const ShoppingHeader = () => {
-  return (
-    <div className=''>
-        shopping Header
-    </div>
-  )
-}
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
 
-export default ShoppingHeader
+  return (
+    <header className=" sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to="/shop/home" className="flex items-center gap-2">
+          <House className="h-6 w-6" />
+          <span className=" font-bold">E-Comarce</span>
+        </Link>
+
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+              <Menu />
+              <span className=" sr-only">Toggle Header menu</span>
+            </Button>
+          </SheetTrigger>
+
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItems />
+            <HeaderRightContent />
+          </SheetContent>
+        </Sheet>
+
+        <div className="hidden lg:block">
+          <MenuItems />
+        </div>
+
+        <div className=" hidden lg:block">
+          <HeaderRightContent />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default ShoppingHeader;
