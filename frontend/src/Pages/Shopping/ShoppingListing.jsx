2b89a01f855625fd1dfbfe1d28e12903c467f71b
Himanshu Verma <kumarsurajverma6001@gmail.com>
2024-09-10 21:35:05 +0530

    minnor fix of filtering

--- frontend/src/Pages/Shopping/ShoppingListing.jsx
+++ frontend/src/Pages/Shopping/ShoppingListing.jsx
@@ -1,5 +1,15 @@
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { sortOptions } from '@/config/Index';
 import { ProductFilter, ShopProductTile } from '@/Index';
 import { fetchAllFilteredProducts } from '@/store/Slices/Shopping/ShoppingProductSlice';
+import { ArrowUpDownIcon } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
@@ -9,12 +19,12 @@ const createSearchParamsHelper = (filterParams) => {
 
   for (const [key, value] of Object.entries(filterParams)) {
     if (Array.isArray(value) && value.length > 0) {
-      const paramValue = value.join(",");
+      const paramValue = value.join(',');
       queryParams.push(`${key}=${encodeURIComponent(paramValue)}`);
     }
   }
 
-  return queryParams.join("&");
+  return queryParams.join('&');
 };
 
 // ShoppingListing component
@@ -25,7 +35,7 @@ const ShoppingListing = () => {
   const [sort, setSort] = useState(null);
   const [searchParams, setSearchParams] = useSearchParams();
 
-  const categorySearchParam = searchParams.get("category");
+  const categorySearchParam = searchParams.get('category');
 
   function handleSort(value) {
     setSort(value);
@@ -50,12 +60,12 @@ const ShoppingListing = () => {
     }
 
     setFilters(cpyFilters);
-    sessionStorage.setItem("filters", JSON.stringify(cpyFilters));
+    sessionStorage.setItem('filters', JSON.stringify(cpyFilters));
   }
 
   useEffect(() => {
-    setSort("price-lowtohigh");
-    setFilters(JSON.parse(sessionStorage.getItem("filters")) || {});
+    setSort('price-lowtohigh');
+    setFilters(JSON.parse(sessionStorage.getItem('filters')) || {});
   }, [categorySearchParam]);
 
   useEffect(() => {
@@ -73,20 +83,50 @@ const ShoppingListing = () => {
     }
   }, [dispatch, sort, filters]);
 
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
       <ProductFilter filters={filters} filterHandler={filterHandler} />
       <div className="bg-background w-full rounded-lg shadow-sm">
         <div className="p-4 border-b flex items-center justify-between">
           <h2 className="text-lg font-extrabold">All Products</h2>
+          <div className="flex items-center gap-3">
+            <span className="text-muted-foreground">
+              {productList?.length} Products
+            </span>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  className="flex items-center gap-1"
+                >
+                  <ArrowUpDownIcon className="h-4 w-4" />
+                  <span>Sort by</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-[200px]">
+                <DropdownMenuRadioGroup value={sort} onValueChange={handleSort}>
+                  {sortOptions.map((sortItem) => (
+                    <DropdownMenuRadioItem
+                      value={sortItem.id}
+                      key={sortItem.id}
+                    >
+                      {sortItem.label}
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-          {productList && productList.length > 0
-            ? productList.map((productItem) => (
-                <ShopProductTile key={productItem._id} product={productItem} />
-              ))
-            : <p>No products found.</p>}
+          {productList && productList.length > 0 ? (
+            productList.map((productItem) => (
+              <ShopProductTile key={productItem._id} product={productItem} />
+            ))
+          ) : (
+            <p>No products found.</p>
+          )}
         </div>
       </div>
     </div>
